865e723b4d261a33bfe5c4f996a85b502bb2a90a
mnttea <mntteass@gmail.com>
2020-07-14 22:54:45 -0700

    added dashboard

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
 	AUTH_ERROR,
 	LOGIN_SUCCESS,
 	LOGIN_FAIL,
-	LOGOUT
+	LOGOUT,
+	CLEAR_PROFILE
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -88,9 +89,8 @@ export const login = (email, password) => async dispatch => {
 	}
 };
 
-// logout user
+// logout user / Clear Profile
 export const logout = () => dispatch => {
-	dispatch({
-		type: LOGOUT
-	});
+	dispatch({ type: LOGOUT });
+	dispatch({ type: CLEAR_PROFILE });
 };
--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Alert from './components/layout/Alert';
-import Landing from './components/layout/Landing';
-import Navbar from './components/layout/Navbar';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Alert from '../components/layout/Alert';
+import Landing from '../components/layout/Landing';
+import Navbar from '../components/layout/Navbar';
+import Register from '../components/auth/Register';
+import Login from '../components/auth/Login';
+import Dashboard from '../components/dashboard/Dashboard';
+import PrivateRoute from '../components/routing/PrivateRoute';
 
 // redux
 import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import store from '../store';
+import { loadUser } from '../actions/auth';
+import setAuthToken from '../utils/setAuthToken';
 
-import './App.css';
+import '../css/App.css';
 
 if (localStorage.token) {
 	setAuthToken(localStorage.token);
@@ -35,6 +37,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						</Switch>
 					</section>
 				</>
