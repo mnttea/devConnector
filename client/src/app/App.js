0374224524bca472072fb1093a8aa4427d81be21
mnttea <mntteass@gmail.com>
2020-07-17 20:24:57 -0700

    implement add experience and add education

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -6,6 +6,10 @@ import Navbar from '../components/layout/Navbar';
 import Register from '../components/auth/Register';
 import Login from '../components/auth/Login';
 import Dashboard from '../components/dashboard/Dashboard';
+import CreateProfile from '../components/profile-forms/CreateProfile';
+import EditProfile from '../components/profile-forms/EditProfile';
+import AddExperience from '../components/profile-forms/AddExperience';
+import AddEducation from '../components/profile-forms/AddEducation';
 import PrivateRoute from '../components/routing/PrivateRoute';
 
 // redux
@@ -38,6 +42,22 @@ const App = () => {
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
 							<PrivateRoute exact path='/dashboard' component={Dashboard} />
+							<PrivateRoute
+								exact
+								path='/create-profile'
+								component={CreateProfile}
+							/>
+							<PrivateRoute exact path='/edit-profile' component={EditProfile} />
+							<PrivateRoute
+								exact
+								path='/add-experience'
+								component={AddExperience}
+							/>
+							<PrivateRoute
+								exact
+								path='/add-education'
+								component={AddEducation}
+							/>
 						</Switch>
 					</section>
 				</>
