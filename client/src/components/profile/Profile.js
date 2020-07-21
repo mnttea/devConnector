24efe93ba3fe5f4a0b1e370c6c4b88b9ebfa8fe6
mnttea <mntteass@gmail.com>
2020-07-21 10:34:15 -0700

    implement display experience, education, and github repos, in profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,9 @@ import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience.js';
+import ProfileEducation from './ProfileEducation.js';
+import ProfileGithub from './ProfileGithub.js';
 import { Link } from 'react-router-dom';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
@@ -28,9 +31,43 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
 								Edit Profile
 							</Link>
 						)}
-					<div class='profile-grid my-1'>
+					<div className='profile-grid my-1'>
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<>
+									{profile.experience.map(experience => (
+										<ProfileExperience
+											key={experience._id}
+											experience={experience}
+										/>
+									))}
+								</>
+							) : (
+								<h4>No experience credentials</h4>
+							)}
+						</div>
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Education</h2>
+							{profile.education.length > 0 ? (
+								<>
+									{profile.education.map(education => (
+										<ProfileEducation
+											key={education._id}
+											education={education}
+										/>
+									))}
+								</>
+							) : (
+								<h4>No education credentials</h4>
+							)}
+						</div>
+
+						{profile.githubusername && (
+							<ProfileGithub username={profile.githubusername} />
+						)}
 					</div>
 				</>
 			)}
