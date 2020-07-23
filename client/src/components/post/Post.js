1722f4245262df1c27abfa2ef4a4cbe2d94fc77e
mnttea <mntteass@gmail.com>
2020-07-22 18:30:57 -0700

    implement comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,11 +5,13 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
 	useEffect(() => {
 		getPost(match.params.id);
-	}, [getPost]);
+	}, [getPost, match.params.id]);
 	return loading || post === null ? (
 		<Spinner />
 	) : (
@@ -18,6 +20,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
 				Back to Posts
 			</Link>
 			<PostItem post={post} showActions={false} />
+			<CommentForm postId={post._id} />
+			<div className='comments'>
+				{post.comments.map(comment => (
+					<CommentItem key={comment._id} comment={comment} postId={post._id} />
+				))}
+			</div>
 		</>
 	);
 };
